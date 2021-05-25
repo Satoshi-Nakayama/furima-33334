60543764652a0891c0ce19ba3767a06f17a95919
Satoshi-Nakayama <s91.satoshi.nakayama@gmail.com>
2021-05-25 17:56:51 +0900

    購入機能実装　修正(2回目)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :current_user_check, only: [:edit, :update, :destroy]
-
+  before_action :order_check, only: [:edit, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -24,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.order != nil
-      redirect_to root_path
-    end
   end
 
   def update
@@ -55,6 +52,12 @@ class ItemsController < ApplicationController
 
   def current_user_check
     if current_user != @item.user
+      redirect_to root_path
+    end
+  end
+
+  def order_check
+    if @item.order != nil
       redirect_to root_path
     end
   end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '購入情報の保存' do
     before do
-      @order_address = FactoryBot.build(:order_address)
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+      sleep 0.1
     end
 
     it '必要な情報を適切に入力すると購入できる' do
